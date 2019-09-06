996407390f27afa05482fa2909ec17ce6babd95b
Anderson <anderson@sinprosp.org.br>
2019-09-06 08:37:36 -0300

    Entendendo testes no laravel

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Thread;
+use App\User;
 use Faker\Generator as Faker;
 
 /*
@@ -19,5 +21,27 @@ $factory->define(App\User::class, function (Faker $faker) {
         'email' => $faker->unique()->safeEmail,
         'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
         'remember_token' => str_random(10),
+    ];
+});
+
+$factory->define(App\Thread::class, function (Faker $faker) {
+    return [
+        'title' => $faker->sentence,
+        'body' => implode(' ', $faker->paragraphs),
+        'user_id' => function () {
+            return factory(User::class)->create()->id;
+        }
+    ];
+});
+
+$factory->define(App\Reply::class, function (Faker $faker) {
+    return [
+        'body' => $faker->paragraph,
+        'user_id' => function () {
+            return factory(User::class)->create()->id;
+        },
+        'thread_id' => function () {
+            return factory(Thread::class)->create()->id;
+        }
     ];
 });
